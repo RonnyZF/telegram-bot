6c19ae76dd588327701e2222f1bd2f5dc4b3bee7
ronnyzf <ronnyzarate@gmail.com>
2019-06-14 17:04:43 -0500

    update

--- graphLib.py
+++ graphLib.py
@@ -3,16 +3,14 @@ import datetime
 import csv
 import itertools
 import matplotlib.pyplot as plt
+import scipy as sp
+import scipy.signal as signal
 
 now = datetime.datetime.now()
-tiempo=[]
-temperatura=[]
-humedad=[]
-distancia=[]
-luminosidad=[]
-
 
 def graficar_temp():
+    tiempo=[]
+    temperatura=[]
     with open('data.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
@@ -31,17 +29,20 @@ def graficar_temp():
     #plt.show()
 
 def graficar_nivel():
+    tiempo=[]
+    distancia=[]
     with open('data.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             time=float(row[4])
             dist=float(row[1])
-            #tiempo.append(time)
+            tiempo.append(time)
             distancia.append(dist)
+    distancia_f=sp.signal.medfilt(distancia,21)
     print(len(tiempo))
-    print(len(distancia))
+    print(len(distancia_f))
     fig3=plt.figure(3)
-    plt.plot(tiempo,distancia, 'm')
+    plt.plot(tiempo,distancia_f, 'm')
     plt.xlabel('Tiempo (s)')
     plt.ylabel('Nivel ')
     plt.title('Nivel de alimento')
@@ -52,12 +53,14 @@ def graficar_nivel():
 
 
 def graficar_hum():
+    tiempo=[]
+    humedad=[]
     with open('data.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             time=float(row[4])
             humed=float(row[2])
-            #tiempo.append(time)
+            tiempo.append(time)
             humedad.append(humed)
     fig2=plt.figure(2)
     plt.plot(tiempo,humedad,'m')
@@ -69,13 +72,14 @@ def graficar_hum():
     #plt.show()
 
 def graficar_luminosidad():
-
+    tiempo=[]
+    luminosidad=[]
     with open('data.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             time=float(row[4])
             lum=float(row[3])
-            #tiempo.append(time)
+            tiempo.append(time)
             luminosidad.append(lum)
     fig4=plt.figure(4)
     plt.plot(tiempo,luminosidad, 'm')
